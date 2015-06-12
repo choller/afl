116889eb78c5ff5706578002bd75a795c1c1a230
Christian Holler (:decoder) <choller@mozilla.com>
2015-06-12 15:25:31 +0200

    Add AFL_DEBUG_CHILD_OUTPUT for debugging purposes

--- afl-fuzz.c
+++ afl-fuzz.c
@@ -1868,8 +1868,10 @@ static void init_forkserver(char** argv) {
 
     setsid();
 
-    dup2(dev_null_fd, 1);
-    dup2(dev_null_fd, 2);
+    if (!getenv("AFL_DEBUG_CHILD_OUTPUT")) {
+    	dup2(dev_null_fd, 1);
+    	dup2(dev_null_fd, 2);
+    }
 
     if (out_file) {
 
